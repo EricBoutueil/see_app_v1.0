3a6eadd2b15dad1430b799d098894dd2eae26f07
Eric Boutueil <e.boutueil@gmail.com>
2018-03-15 11:13:21 +0100

    has_many, dependent, validations

--- app/models/harbour.rb
+++ app/models/harbour.rb
@@ -1,2 +1,7 @@
 class Harbour < ApplicationRecord
+  has_many :movements, dependent: :nullify # custom TBD for archive
+
+  validates :country, presence: true
+  validates :name, presence: true, uniqueness: true
+  validates :address, presence: true, uniqueness: true
 end
--- app/models/movement.rb
+++ app/models/movement.rb
@@ -1,4 +1,9 @@
 class Movement < ApplicationRecord
   belongs_to :harbour
   belongs_to :type
+
+  validates :date, presence: true, uniqueness: { scope: [:harbour, :type ],
+    message: "already exists for harbour and flow" }
+  validates :volume, presence: true, numericality: true
+
 end
