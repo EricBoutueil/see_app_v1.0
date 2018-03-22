ea83a6ba0cef90d89e0c76f46f1b34bb602f8746
Eric Boutueil <e.boutueil@gmail.com>
2018-03-22 11:55:54 +0100

    circles implemented

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -1,26 +1,47 @@
-// fichier de def d'execution (pas de déf)
+// function definitions
+
+function getCircle(totalvolume) {
+  return {
+    path: google.maps.SymbolPath.CIRCLE,
+    fillColor: 'blue',
+    fillOpacity: .8,
+    scale: 10, // = MAX_TOTALVOLUME --> calc < 20 ? -> each totalvolume range between max harbour 8e7 -> 4e6 // 5e06 -> 3e5 // 100 -> need max
+    strokeColor: 'blue',
+    strokeWeight: 1
+  };
+}
+
+
+// direct execution
 
 var map;
-
-
-// var jsonparsed;
-// jsonparsed = JSON.parse(mapElement.dataset.geojson);
 
 console.log("starting map building");
 // va chercher un element dans le DOM, donc faut un DOM chargé, prendre le bon element
 var mapElement = document.getElementById('map');
 
 if (mapElement) {
+  // STEP 1
   map = new google.maps.Map(mapElement, {
         zoom: 6,
         center: {lat: 43.3, lng: 5.4}
       });
 
-  // map.data.addGeoJson(jsonparsed);
+  // STEP 2
+  var jsonparsed = JSON.parse(mapElement.dataset.geojson);
+  map.data.addGeoJson(jsonparsed);
 
-  console.log(map);
-  console.log(jsonparsed);
+  // STEP 3
+  map.data.setStyle(function(feature) {
+    var totalvolume = feature.getProperty('totvol');
+    return {
+      icon: getCircle(totalvolume)
+    };
+  });
+
 }
 else { console.log("there is no mapElement")};
+
+
 
 // event listener sur map created ??
