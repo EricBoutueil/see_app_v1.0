3187776465a77010b07539ae28102586e3a9f813
Eric Boutueil <e.boutueil@gmail.com>
2018-03-29 23:58:19 +0200

    policy admin except indexes + admin movements index.html.erb

--- app/policies/movement_policy.rb
+++ app/policies/movement_policy.rb
@@ -8,27 +8,29 @@ class MovementPolicy < ApplicationPolicy
     end
   end
 
+  def index?
+    user_is_admin?
+  end
+
   def edit?  # => update
-    user_is_owner?
+    user_is_admin?
   end
 
   def create? # => new
-    return true
+    user_is_admin?
   end
 
   def update?
-    user_is_owner?
+    user_is_admin?
   end
 
   def destroy?
-    user_is_owner?
+    user_is_admin?
   end
 
   private
 
-  def user_is_owner?
-    record.user == user
-    # - record: the restaurant passed to the `authorize` method in controller
-    # - user:   the `current_user` signed in with Devise.
+  def user_is_admin?
+    user.admin
   end
 end
