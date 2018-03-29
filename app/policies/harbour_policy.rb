85d3a9ccb7b6d370081b84e0525c8069d1abb463
Eric Boutueil <e.boutueil@gmail.com>
2018-03-29 23:10:08 +0200

    policy harbour index

---
+++ app/policies/harbour_policy.rb
@@ -0,0 +1,36 @@
+class HarbourPolicy < ApplicationPolicy
+  class Scope < Scope # for index
+    def resolve
+      scope.where.not(latitude: nil, longitude: nil)
+      # scope ~ Restaurant
+      # For a multi-tenant SaaS app, you may want to use:
+      # scope.where(user: user)
+    end
+  end
+
+  def edit?  # => update
+    user_is_owner?
+  end
+
+  def create? # => new
+    return true
+  end
+
+  def update?
+    user_is_owner?
+  end
+
+  def destroy?
+    user_is_owner?
+  end
+
+  private
+
+  def user_is_owner?
+    record.user == user
+    # - record: the restaurant passed to the `authorize` method in controller
+    # - user:   the `current_user` signed in with Devise.
+  end
+end
+
+
