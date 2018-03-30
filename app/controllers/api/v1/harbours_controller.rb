308d52b03e934d36284d6b2e444a443f3308117c
Eric Boutueil <e.boutueil@gmail.com>
2018-03-30 11:41:56 +0200

    geojson built with jbuilder OKKKKK

--- app/controllers/api/v1/harbours_controller.rb
+++ app/controllers/api/v1/harbours_controller.rb
@@ -1,5 +1,20 @@
 class Api::V1::HarboursController < Api::V1::BaseController
+
   def index
     @harbours = policy_scope(Harbour)
+    @selected_harbours = []
+    if (params[:name])
+      # @harbours = @harbours.where(name: params[:name]) #simple filter
+      @selected_harbours << @harbours.where(name: params[:name]).first #select2
+    end
   end
+
+    # if deleted-name
+
+# private
+
+#   def harbour_params
+#     params.require(:harbour).permit(:name)
+#   end
+
 end
--- app/controllers/harbours_controller.rb
+++ app/controllers/harbours_controller.rb
@@ -2,34 +2,13 @@ class HarboursController < ApplicationController
 
   skip_before_action :authenticate_user!, only: [:index]
 
-
   def index
-    @harbours = policy_scope(Harbour)
-
-    if (params[:name])
-      @harbours = @harbours.where(name: params[:name])
+    respond_to do |format|
+      format.html
+      format.js  # <-- will render `app/views/harbours/index.js.erb`
     end
-
-    # binding.pry
-    @features = @harbours.map do |harbour|
-      {
-        "type": "Feature", #1 feature ~ 1 harbour where (movements.filter).sum
-        "properties": {
-          "totvol": 123123 # harbour.movements.types.where(flow: flow, code: code) # total sum to calculate
-        },
-        "geometry": {
-          "type": "Point",
-          "coordinates": [harbour.longitude, harbour.latitude]
-        }#,
-        #"id": "usc000csx3"
-      }
-    end
-
-    @geojson =
-      {
-        "type": "FeatureCollection",
-        "features": @features
-      }
-
+    # redirect == router - controller - view
+    # render == view directly
   end
+
 end
