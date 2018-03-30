afb458ce5f0a5cd529040aac1f4c6003fea18b43
Eric Boutueil <e.boutueil@gmail.com>
2018-03-30 17:45:47 +0200

    wip - get ajax call working, missing update selected_harbours

--- app/javascript/components/select2.js
+++ app/javascript/components/select2.js
@@ -7,27 +7,19 @@ $('#select2_harbours').select2({
     placeholder: "Ecrivez ou sélectionnez pour filtrer",
     allowClear: true
 });
-
 // Requiring CSS == importing CSS! Path is relative to ./node_modules
-// -> css included in js (not in asset pipeline) and available in / compiled by Webpack
+// CSS included in JS (not in asset pipeline) and available in / compiled by Webpack
 import 'select2/dist/css/select2.css';
-
 
 // event listener
 $('#select2_harbours').on("select2:select", (event) => {
   var harbourSelected = event.params.data.text;
-  console.log(harbourSelected); // --> bayonne
-
-// call AJAX == fetch
-// fetch('http://localhost:3000/api/v1/restaurants/1').then((response) => {
-//   .then(response => response.json()) // lit le json pour être bien interprété par js
-//   .then((data) => { // début du call back pour faire mon traitement
-//       console.log(data);
-//       // data.results.forEach((person) => {
-//       //   const item = `<li>${person.name}</li>`;
-//       //   people.insertAdjacentHTML("beforeend", item);
-//       // });
-//   });
-// });
-
+  console.log(harbourSelected); // -> bayonne  -> OK
+  // call AJAX
+  $.get({
+    url: '/harbours', // /api/v1/harbours
+    data: {name: harbourSelected}
+  }); // rajouter fonction de call back ?
+// requete ajax doit avoir json en response, passe dans call back et y manipule cet object + 2eme function en cas d'erreur
+// .done et .fail
 });
