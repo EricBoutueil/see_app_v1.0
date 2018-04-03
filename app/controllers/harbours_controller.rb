3cf245d6f4d9d93bbcf48fe3d4c9cbf0fbdfa542
Eric Boutueil <e.boutueil@gmail.com>
2018-04-04 00:02:00 +0200

    refacto filter harbour with def

--- app/controllers/harbours_controller.rb
+++ app/controllers/harbours_controller.rb
@@ -6,37 +6,8 @@ class HarboursController < ApplicationController
   def index
     @harbours = policy_scope(Harbour)
 
-  # binding.pry
+    @selected_harbours = Harbour.filter_by_harbour(params, @harbours)
 
-    @selected_harbours = []
-    if (params[:name])
-      params[:name].each do |harbour|
-        @selected_harbours << @harbours.where(name: harbour).first
-      end
-    end
-
-    # binding.pry
-
-# http://localhost:3000/harbours?name%5B%5D=bastia&name%5B%5D=bayonne&_=1522768541831
-
-    if @selected_harbours == []
-      @features = @harbours.map do |harbour|
-        {
-          "type": "Feature", #1 feature ~ 1 harbour where (movements.filter).sum
-          "properties": {
-            "country": harbour.country,
-            "name": harbour.name,
-            "address": harbour.address,
-            "totvol": 123123 # harbour.movements.types.where(flow: flow, code: code) # total sum to calculate
-          },
-          "geometry": {
-            "type": "Point",
-            "coordinates": [harbour.longitude, harbour.latitude]
-          },
-          "id": harbour.id
-        }
-      end
-    else
       @features = @selected_harbours.map do |harbour|
         {
           "type": "Feature", #1 feature ~ 1 harbour where (movements.filter).sum
@@ -53,7 +24,6 @@ class HarboursController < ApplicationController
           "id": harbour.id
         }
       end
-    end
 
     @geojson =
       {
