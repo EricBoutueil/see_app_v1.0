471318051098fa8e0241180217b9bf7d4d129157
Eric Boutueil <e.boutueil@gmail.com>
2018-04-03 23:18:54 +0200

    no harbour filter index ok

--- app/controllers/harbours_controller.rb
+++ app/controllers/harbours_controller.rb
@@ -19,21 +19,40 @@ class HarboursController < ApplicationController
 
 # http://localhost:3000/harbours?name%5B%5D=bastia&name%5B%5D=bayonne&_=1522768541831
 
-    @features = @selected_harbours.map do |harbour|
-      {
-        "type": "Feature", #1 feature ~ 1 harbour where (movements.filter).sum
-        "properties": {
-          "country": harbour.country,
-          "name": harbour.name,
-          "address": harbour.address,
-          "totvol": 123123 # harbour.movements.types.where(flow: flow, code: code) # total sum to calculate
-        },
-        "geometry": {
-          "type": "Point",
-          "coordinates": [harbour.longitude, harbour.latitude]
-        },
-        "id": harbour.id
-      }
+    if @selected_harbours == []
+      @features = @harbours.map do |harbour|
+        {
+          "type": "Feature", #1 feature ~ 1 harbour where (movements.filter).sum
+          "properties": {
+            "country": harbour.country,
+            "name": harbour.name,
+            "address": harbour.address,
+            "totvol": 123123 # harbour.movements.types.where(flow: flow, code: code) # total sum to calculate
+          },
+          "geometry": {
+            "type": "Point",
+            "coordinates": [harbour.longitude, harbour.latitude]
+          },
+          "id": harbour.id
+        }
+      end
+    else
+      @features = @selected_harbours.map do |harbour|
+        {
+          "type": "Feature", #1 feature ~ 1 harbour where (movements.filter).sum
+          "properties": {
+            "country": harbour.country,
+            "name": harbour.name,
+            "address": harbour.address,
+            "totvol": 123123 # harbour.movements.types.where(flow: flow, code: code) # total sum to calculate
+          },
+          "geometry": {
+            "type": "Point",
+            "coordinates": [harbour.longitude, harbour.latitude]
+          },
+          "id": harbour.id
+        }
+      end
     end
 
     @geojson =
