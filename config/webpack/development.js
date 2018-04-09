c43e6b4af8e39915020850145e9de02bf64a9a5f
Eric Boutueil <e.boutueil@gmail.com>
2018-04-09 11:19:15 +0200

    commit before restart from stratch

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,3 +1,12 @@
 const environment = require('./environment')
 
-module.exports = environment.toWebpackConfig()
+// module.exports = environment.toWebpackConfig()
+
+
+module.exports = {
+mode: 'development'
+  plugins: [
+    new webpack.NamedModulesPlugin(),
+    new webpack.DefinePlugin({ "process.env.NODE_ENV": JSON.stringify("development") }),
+  ]
+}
